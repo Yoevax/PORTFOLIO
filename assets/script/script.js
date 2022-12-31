e82356ed22f871aa2975d5df4c7b642bb6fafa9a
Yousra Lemqaddem <lemqaddem.yousra@gmail.com>
2022-12-31 21:14:33 +0100

    first commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,63 @@
-// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-//   anchor.addEventListener('click', function (e) {
-//       e.preventDefault();
 
-//       document.querySelector(this.getAttribute('href')).scrollIntoView({
-//           behavior: 'smooth'
-//       });
-//   });
+const trailer = document.getElementById("trailer");
+
+const animateTrailer = (e, interacting) => {
+  const x = e.clientX - trailer.offsetWidth / 2,
+        y = e.clientY - trailer.offsetHeight / 2;
+  
+  const keyframes = {
+    transform: `translate(${x}px, ${y}px) scale(${interacting ? 8 : 1})`
+  }
+  
+  trailer.animate(keyframes, { 
+    duration: 800, 
+    fill: "forwards" 
+  });
+}
+
+const getTrailerClass = type => {
+  switch(type) {
+    case "video":
+      return "fa-solid fa-play";
+    default:
+      return "fa-solid fa-arrow-up-right"; 
+  }
+}
+
+window.onmousemove = e => {
+  const interactable = e.target.closest(".interactable"),
+        interacting = interactable !== null;
+  
+  const icon = document.getElementById("trailer-icon");
+  
+  animateTrailer(e, interacting);
+  
+  trailer.dataset.type = interacting ? interactable.dataset.type : "";
+  
+  if(interacting) {
+    icon.className = getTrailerClass(interactable.dataset.type);
+  }
+}
+// let deesse = document.getElementById("deesse");
+// window.addEventListener("scroll", ()=>{
+//     deesse.style.transform = "rotate(-20deg)";
+// })
+
+
+// let mesbtn = document.getElementById('mesBtn');
+// var windowHeight = window.innerHeight;
+// var windowWidth = window.innerWidth;
+// var scrollArea = 1000 - windowHeight;
+// var square1 = document.getElementsByClassName('btn')[0];
+// var square2 = document.getElementsByClassName('btn')[1];
+
+// // update position of square 1 and square 2 when scroll event fires.
+// window.addEventListener('scroll', function() {
+//   var scrollTop = window.pageYOffset || window.scrollTop;
+//   var scrollPercent = scrollTop/scrollArea || 0;
+  
+//   square1.style.top = scrollPercent*window.innerWidth + 'px';
+//   square2.style.top = 800 - scrollPercent*window.innerWidth*0.6 + 'px';
 // });
 
 // const mouseCursor = document.getElementById("mouse_cursor");
